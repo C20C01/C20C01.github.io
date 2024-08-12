88b623f1d44db807805f50511103007f08b88838
CC <cc_20_01@163.com>
2024-08-12 20:02:21 +0800

    Site updated: 2024-08-12 20:02:17

--- Mods/MusicBox/Tool/i18n.js
+++ Mods/MusicBox/Tool/i18n.js
@@ -25,16 +25,16 @@ const contents = {
     "welcome": ["🤗 欢迎使用nbs文件导出工具", "🤗 Welcome to nbs file export tool"],
     "learnMore": ["[了解更多]", "[Learn More]"],
     "description": ["本工具为模组“纸带八音盒”的配套工具，用于将nbs文件导出至书与笔供模组使用。", "This tool is a supporting tool for the mod \"Music Box\", used to export nbs file to Book and Quill for the mod to use."],
-    "notes": ["🔔 温馨提示：", "🔔 Notes:"],
+    "tips": ["🔔 提示", "🔔 Tips"],
     "tip1": ["不支持旧版nbs格式，请使用新版Note Block Studio保存文件以更新格式。", "Old version of nbs file is not supported, please use the new version of Note Block Studio to save the file to update the format."],
     "tip2": ["不支持自定义音色，请将自定义音色替换为默认音色后再导出。", "Custom timbre is not supported, please replace custom timbre with default timbre before exporting."],
-    "tip3": ["使用命令模式时，将生成会给予你（@p）已经输入好内容的书与笔的命令，将其复制到命令方块中以使用。", "In \"Command\" mode, it will generate a command to give you(@p) a specific Book and Quill, copy it to command block to use."],
-    "tip4": ["使用逐页导出模式时，将会把需要输入至书与笔中的内容在此处列出，你需要手动逐页复制到书与笔中。", "In \"Page-by-page\" mode, it will list the content that needs to be input into the Book and Quill here, you need to manually copy it page by page."],
+    "tip3": ["使用命令模式时，将会生成给予你（@p）已经输入好内容的书与笔的命令，将其复制到命令方块中以使用。", "In \"Command\" mode, it will generate a command to give you(@p) a specific Book and Quill, copy it to command block to use."],
+    "tip4": ["使用逐页导出模式时，将会把需要输入至书与笔中的内容在此处列出，你需要将其逐页复制到书与笔中。", "In \"Page-by-page\" mode, it will list the content that needs to be input into the Book and Quill here, you need to copy it page by page."],
     "tip5": ["点击生成的文本即可将其复制至剪切板。", "Click the generated text to copy it to the clipboard."],
     "defaultResult": ["将 NBS 文件导出至书与笔以方便后续使用。访问 https://github.com/C20C01/MusicBox 了解更多关于模组\"纸带八音盒\"的信息。", "Export NBS file to Book and Quill for further use. Visit https://github.com/C20C01/MusicBox to find more about the mod \"Music Box\"."],
 }
 
-const welcomeHtml = '<div style="color:white"><b><span accesskey="welcome"></span></b><p><span accesskey="description"></span><a href="https://github.com/C20C01/MusicBox"><span accesskey="learnMore"></span></a></p><br><br><br><b><span accesskey="notes"></span></b><ul><li><span accesskey="tip1"></span><a href="https://opennbs.org/">[OPEN NOTE BLOCK STUDIO]</a></li><li><span accesskey="tip2"></span></li><li><span accesskey="tip3"></span></li><li><span accesskey="tip4"></span></li><li><span accesskey="tip5"></span></li></ul></div>';
+const welcomeHtml = '<div style="color:white"><b><span accesskey="welcome"></span></b><p><span accesskey="description"></span><a href="https://github.com/C20C01/MusicBox"><span accesskey="learnMore"></span></a></p><br><br><br><b><span accesskey="tips"></span></b><ul><li><span accesskey="tip1"></span><a href="https://opennbs.org/">[OPEN NOTE BLOCK STUDIO]</a></li><li><span accesskey="tip2"></span></li><li><span accesskey="tip3"></span></li><li><span accesskey="tip4"></span></li><li><span accesskey="tip5"></span></li></ul></div>';
 const langList = ["zh-CN", "en-US"];
 
 document.addEventListener("DOMContentLoaded", initLang);
